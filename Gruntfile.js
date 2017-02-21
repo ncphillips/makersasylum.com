1e7ebadf054176f061a5dd941f64be2acf38e5a2
Akshat Kedia <akshat.kedia@outlook.com>
2017-02-21 20:36:44 +0530

    updated membership design

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        src: ['scripts/_source/jquery.min.js', 'scripts/_source/bootstrap.min.js', 'scripts/_source/main.js'],
+        dest: 'scripts/all.js',
+      },
+    },
+    uglify: {
+      my_target: {
+        files: {
+          'scripts/all.js': ['scripts/all.js']
+        }
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['scripts/_source/*.js'],
+        tasks: ['concat'],
+        options: {
+          livereload: true,
+        },
+      },
+    },
+  });
+
+  // Load plugins.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Register tasks.
+  grunt.registerTask('default', ['concat', 'uglify']);
+  grunt.registerTask('dev', ['concat', 'watch']);
+
+};
